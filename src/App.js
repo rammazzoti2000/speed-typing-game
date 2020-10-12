fc2b0ef866ce89a20d3df9230255b333c0121cda
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-10-12 13:44:43 +0200

    Implement useState hook to update state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [ words, getWord ] = useState("");
+  console.log(words);
+
+  function handleChange(event) {
+      const { value } = event.target;
+      getWord({
+        value
+      })
+  }
+
   return (
     <div className="App">
       <h1>Type Mama!!</h1>
       <form>
-        <textarea />
+        <textarea value={words.value} onChange={handleChange} />
       </form>
       <h4>Time remaining: ???</h4>
       <button>Start</button>
