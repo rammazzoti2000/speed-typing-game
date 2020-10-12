1615478e6d38144fdf287cd85abcfbbd58518be0
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-10-12 13:04:02 +0200

    Add basic structure and styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>I am right here</h1>
+      <h1>Type Mama!!</h1>
+      <form>
+        <textarea />
+      </form>
+      <h4>Time remaining: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
